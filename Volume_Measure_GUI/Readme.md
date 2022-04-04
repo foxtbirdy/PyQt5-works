98abbeff6b8e820f4d40c6da10784715e165475c
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2022-04-04 20:24:55 +0600

    Final Commit. [I don't care of the names I gave on this project....LOL]

--- Volume_Measure_GUI/Readme.md
+++ Volume_Measure_GUI/Readme.md
@@ -1,10 +1,15 @@
 # Volume Measurement GUI
 
 
-
 Is a demo app that evaluates the value of a sound in a dialer based on its current position as a value. 
 
 This is a simple app that is also used for research and references. Feel free to use this as a method of managing multiple widgets.
 
+Note that the application may not fully respect the [K.I.S.S](https://en.wikipedia.org/wiki/KISS_principle) principle. I just did what I had in mind.
 
-Note that the application may not fully respect the K.I.S.S principle. I just did what I had in mind.
+Thanks to *GeekforGeeks* for the project idea.
+
+
+Demo - [Twitter](https://twitter.com/Black_2_white/status/1510985873080614912)
+
+Download this folder [here using Downgit](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Code-Blender-7/PyQt5-works/tree/main/Volume_Measure_GUI).
--- Volume_Measure_GUI/app.py
+++ Volume_Measure_GUI/app.py
@@ -2,7 +2,7 @@
 # @Author: Climax
 # @Date:   2022-04-03 20:45:03
 # @Last Modified by:   Climax
-# @Last Modified time: 2022-04-04 17:36:28
+# @Last Modified time: 2022-04-04 19:30:18
 
 
 
@@ -17,28 +17,33 @@ class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
-		self.setWindowTitle("My App. A Dial Wheel")
+		self.setWindowTitle("Volume Evaluation")
 		self.setFixedSize(600, 400)
 		self.Volume_Evaluation()
+		self.dial()
 
-		widget = QDial(self)
-		
 
-		widget.setGeometry(100,100,150,200) # x,y,w,h
-		widget.setRange(0,100)
-		widget.setSingleStep(1) # set keyboard input to 1 step
-		widget.setSliderPosition(0) # set pos to 0
-		widget.valueChanged.connect(self.value_Changed)
+	def dial(self):
+		self.widget = QDial(self)
+		self.widget.setGeometry(100,100,150,200) # x,y,w,h
+		self.widget.setRange(0,100)
+		self.widget.setSingleStep(1) # set keyboard input to 1 step
+		self.widget.setSliderPosition(0) # set pos to 0
+		self.widget.valueChanged.connect(self.value_changed)
 
 	def Volume_Evaluation(self):
-		self.text_label = QLabel("testing", self)
-		self.text_label.setGeometry(300,150,200,100) # x,y,w,h
+		self.text_label = QLabel("Move Dial to Evaluate", self)
+		self.text_label.setGeometry(260,150,320,100) # x,y,w,h
 		self.text_label.setFont(QFont('Arial', 15))
 
-	def value_Changed(self, i):
-		self.text_label.setText(f"Current value: {i}")
-			
-
+	def value_changed(self, i):
+		if i==0:  self.text_label.setText(f"Current value[{i}]: Muted")
+		if i>0:self.text_label.setText(f"Current value[{i}]: Very Low")
+		if i>=20: self.text_label.setText(f"Current value[{i}]: Low")
+		if i>=45: self.text_label.setText(f"Current value[{i}]: Medium")
+		if i>=80: self.text_label.setText(f"Current value[{i}]: High")
+		if i==100: self.text_label.setText(f"Current value[{i}]: Max")
+		print(i)
 
 app = QApplication(sys.argv)
 
