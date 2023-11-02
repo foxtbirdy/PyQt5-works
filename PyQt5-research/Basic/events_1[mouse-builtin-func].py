e1f620d2261bd78c211a3d397df779951c6a3a89
FoxtBird <foxtbird7@gmail.com>
2023-11-02 21:23:37 +0600

    Modifed files, Resumed study
    Removed old plugin generated comment blocks. Began resumming work with PyQt5

--- PyQt5-research/Basic/events_1[mouse-builtin-func].py
+++ PyQt5-research/Basic/events_1[mouse-builtin-func].py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-# @Author: Climax
-# @Date:   2022-07-08 18:43:46
-# @Last Modified by:   Climax
-# @Last Modified time: 2022-08-09 20:40:58
-
-
+# Events related to mouse movement and mouse behavior
 
 import sys
 from PyQt5.QtWidgets import (QApplication, QLabel, QMainWindow)
@@ -15,16 +9,14 @@ app = QApplication(sys.argv)
 
 class MainWindow(QMainWindow):
 	"""
-	this is the main window
+	this is the main window.
 	"""
-
-
-
+ 
 	def __init__(self):
 		super().__init__()
 		self.label = QLabel("Click in this window")
 		self.setCentralWidget(self.label)
-		self.setMouseTracking(True) # default = False?
+		self.setMouseTracking(True) # default = False
 
 
 # below are default function names for the mouse features.
--- PyQt5-research/Basic/windows_5.py
+++ PyQt5-research/Basic/windows_5.py
@@ -1,77 +1,42 @@
-# -*- coding: utf-8 -*-
-# @Author: Climax
-# @Date:   2022-07-08 18:43:46
-# @Last Modified by:   Climax
-# @Last Modified time: 2022-08-09 20:39:07
-
-
+# Introduction to Persistant Windows
+# Windows that attains focus from the mainWindow.
 
 import sys
-from random import randint  
+from random import randint
+
 from PyQt5.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget)
 
 
-app = QApplication(sys.argv)
-
+class AnotherWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window % d" % randint(0,100))
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+        
 
 class MainWindow(QMainWindow):
-	"""
-	this is the main window
-	"""
+    def __init__(self):
+        super().__init__()
+        self.w = AnotherWindow()
+        self.button = QPushButton("Push for Window")
+        # self.button.clicked.connect(self.show_new_window)
+        self.button.clicked.connect(self.toggle_window)
+        self.setCentralWidget(self.button)
+        
+    # if window is visible then hide.
+    def toggle_window(self, checked):
+        if self.w.isVisible():
+            self.w.hide()
+        else:
+            self.w.show()
+            
+        
+    def show_new_window(self, checked):
+        self.w.show()
 
-
-
-	def __init__(self):
-		super().__init__()
-		self.w = AnotherWindow()
-		self.button = QPushButton("Click me for a widget to appear")
-		self.button.clicked.connect(self.create_window)
-		self.setCentralWidget(self.button)
-
-	def create_window(self, clicked):		
-		self.w.show()
-
-
-class AnotherWindow(QWidget):
-
-	"""
-	This is the window where there are no parents. it is a standalone widget of a sort
-	"""
-	def __init__(self):
-		super().__init__()
-
-
-		layout = QVBoxLayout()
-		self.label = QLabel(f"Another Window with a random number {randint(0,100)}")
-
-		self.button_exit = QPushButton("Click me to exit")
-		self.button_exit.clicked.connect(self.exit_program)
-
-		layout.addWidget(self.label)
-		layout.addWidget(self.button_exit)
-		
-		self.setLayout(layout)
-
-
-	def exit_program(self):
-		app.quit()
-
-
-
-central = MainWindow()
-central.show()
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
 app.exec_()
-
-
-"""
-
-If you want to see if there is any kind of program that is running or not, be sure to use this codeline 
--> 
-if self.w is None:
-	self.w = AnotherWindow()
-	self.w.show()
-
-
-"""
-
-
